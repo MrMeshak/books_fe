81ab4495abb80d324015ec56226edabe41a3ffdd
MrMeshak <94204153+MrMeshak@users.noreply.github.com>
2023-09-20 18:37:18 +1000

    Adds library store interfaces and types

--- src/store/libraryStore.ts
+++ src/store/libraryStore.ts
@@ -1,3 +1,36 @@
 import { create } from 'zustand';
 
-export interface LibraryStore {}
+export type Filter = { value: 'full'; label: 'Fully Viewable' } | { value: 'partial'; label: 'Partially Viewable' } | { value: 'free-ebooks'; label: 'Free' } | { value: 'paid-ebooks'; label: 'Paid' };
+
+export type Sort = { value: 'relevance'; label: 'relevance' } | { value: 'newest'; label: 'newest' };
+
+export type PrintType = { value: 'all'; label: 'all' } | { value: 'books'; label: 'books' } | { value: 'magazines'; label: 'magazines' };
+
+export interface IStatus {
+  status: 'success' | 'loading' | 'error';
+  message: string;
+}
+
+export interface IPagination {
+  page: number;
+  resultsPerPage: number;
+}
+
+export interface IBook {}
+
+export interface LibraryStore {
+  status: IStatus;
+
+  sort: Sort;
+  filters: Filter[];
+  printType: PrintType;
+
+  pagination: IPagination;
+  searchStr: string;
+
+  books: IBook[];
+
+  actions: {
+    search: () => void;
+  };
+}
